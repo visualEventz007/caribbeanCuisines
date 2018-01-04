f620dff8b7ae30659e07ac8b5dc748751cf1405b
visualEventz007 <31773957+visualEventz007@users.noreply.github.com>
2018-01-04 06:46:02 -0500

    A Caribbean Commit

---
+++ js/global.js
@@ -0,0 +1,22 @@
+﻿//global variable db used
+var db;
+
+//calls when we have problem
+function errorHandler(transaction, error) 
+{
+    alert("SQL error: " + error.message);
+}
+
+
+//the DOM is Ready
+$(document).ready(function () 
+{
+    db = openDatabase('caribbeanCuisineDB', '1.0', 'Our Delicacy dataBase', 5 * 1024 * 1024);
+    createTables();
+
+    getItems();
+
+    getCart();
+    dropCartTable();
+
+});
